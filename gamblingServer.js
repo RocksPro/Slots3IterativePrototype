8785e03bd861088567a27a30d8616a38f13357c5
Stanislav Rodionov <x.rodionov@gmail.com>
2016-06-20 07:56:19 +0300

    CORS активирована.
    Решена проблема доступа к ресурсам сервера с других доменов. Ссылки для
    ревью:
      Сервер: https://slots-3-iterative-prototype-stv909.c9users.io/
      Клиент:
    https://preview.c9users.io/stv909/slots-3-iterative-prototype/gameFronte
    nd.html

--- gamblingServer.js
+++ gamblingServer.js
@@ -3,6 +3,11 @@ const port = 8080;
 
 function handleRequest(request, response)
 {
+	// set CORS headers
+	response.setHeader('Access-Control-Allow-Origin', '*');
+	response.setHeader('Access-Control-Request-Method', '*');
+	response.setHeader('Access-Control-Allow-Methods', 'GET');
+	response.setHeader('Access-Control-Allow-Headers', '*');
 	// treat response as a plain text
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	// finally generate random number from 0 to 9	
